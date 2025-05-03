1780e89433a8aa01f85bf2943dc298de62ebea5e
Farouk Abdi <abdifarouk58@gmail.com>
2025-05-03 18:01:39 +0300

    completed tests

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function focus(event){
+       console.log(event)
+       if(event.type==='focus'){
+        console.log('Good!')
+       }else{
+         console.log("Hey! Eyes on me!")
+       }
+
+    }
+    return(
+        <div>
+            <button onFocus={focus} onBlur={focus}>Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
-    return (
-        <div></div>
-    )
+    function changeInput(event){
+        console.log("Entering password...")
+    }
+  return (
+    <div>
+        <input onChange={changeInput} placeholder="password" type="password"></input>
+    </div>
+  )
 }
 
 export default Keypad;
